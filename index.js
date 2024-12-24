13450f449dd686bc394c651f543422d0b9c93139
David <davirdlessa@gmail.com>
2024-12-24 15:19:38 -0300

    Desafio pronto

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-ffggg
+let nomeDoHeroi = "Lenda"
+let vitoria = 105
+let derrota = 1
+let nivel;
+let resultado = obterSaldoDeRankeadas(vitoria, derrota);
+
+function obterSaldoDeRankeadas(vitoria, derrota){
+    return vitoria - derrota
+}
+
+if (resultado <=10) {
+    nivel = "Ferro"
+} else if (resultado <=20){
+    nivel = "Bronze"
+} else if (resultado <=50){
+    nivel = "Prata"
+} else if (resultado <=70){
+    nivel = "Ouro"
+} else if (resultado <=80){
+    nivel = "Diamante"
+}else if (resultado <=100){
+    nivel = "Lendário"
+} else{
+    nivel = "Imortal"
+}
+console.log(`O heroi ${nomeDoHeroi} tem saldo de vitorias: ${resultado} está no nivel de: ${nivel}.`)
